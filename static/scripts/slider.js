78d5f3d884e1ea2bcc5147684271702d6afa0e0a
Murasambrero <neones@yandex.ru>
2024-08-14 01:30:36 +0800

    add review section

---
+++ static/scripts/slider.js
@@ -0,0 +1,27 @@
+const rewiewsContainer = document.querySelector(".sliders")
+
+const width = rewiewsContainer.scrollWidth;
+console.log(width)
+
+const lengthElement = rewiewsContainer.children.length
+const widthEl = rewiewsContainer.firstElementChild.offsetWidth
+
+const gap = (width - lengthElement * widthEl)/(lengthElement-1)
+
+console.log(`GAP = ${gap}`)
+
+
+let counter = 0;
+const multiplier = widthEl + gap;
+
+function previousReview(){
+    if (counter === 0) return;
+    counter = counter + 1;
+    rewiewsContainer.style.transform = `translateX(${counter * multiplier}px)`;
+}
+
+function nextReview(){
+    if (counter === -(lengthElement-4)) return;
+    counter = counter - 1;
+    rewiewsContainer.style.transform = `translateX(${counter * multiplier}px)`;
+}
